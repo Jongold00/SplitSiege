70b10bb4eebb69945d4b68b3c2e11aab7a1fc71b
KarlB25 <80916563+KarlB25@users.noreply.github.com>
2022-11-01 01:16:18 +0200

    Cleanup of tower prefabs and script names. Clocktower is using new models and spawning in & upgrading correctly.

--- Assets/Scripts/TowerPipeline/Individual Functionality/Behavior/Plasma.cs
+++ Assets/Scripts/TowerPipeline/Individual Functionality/Behavior/Plasma.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class EvilWizard : OffensiveTower
+public class Plasma : OffensiveTower
 {
     [SerializeField]
     GameObject[] spells;
