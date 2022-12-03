93f08f6361c81cfc4e9be52220b81dd1c9055754
KarlB25 <80916563+KarlB25@users.noreply.github.com>
2022-12-03 15:12:29 +0200

    Workshop spawning spiketrap at random point in range after finishing construction

--- Assets/Scripts/TowerPipeline/Individual Functionality/Behavior/Workshop.cs
+++ Assets/Scripts/TowerPipeline/Individual Functionality/Behavior/Workshop.cs
@@ -4,8 +4,42 @@ using UnityEngine;
 
 public class Workshop : OffensiveTower
 {
+    private List<NavNode> navNodesInRange;
+    [SerializeField] GameObject spikeTrap;
+    private bool trapPlaced = false;
+
     public override void Update()
     {
-        // tower logic here
+        if (!CanFire || trapPlaced)
+        {
+            return;
+        }
+
+        navNodesInRange = GetNavNodesInRange();
+        PlaceObjectAtPosition(spikeTrap, navNodesInRange[Random.Range(0, navNodesInRange.Count)].gameObject.transform.position);
     }
+
+    private void PlaceObjectAtPosition(GameObject obj, Vector3 pos)
+    {
+        Instantiate(obj, pos, obj.transform.rotation);
+        trapPlaced = true;
+    }
+
+    private List<NavNode> GetNavNodesInRange()
+    {
+        List<NavNode> nodes = new List<NavNode>();
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, offensiveTowerData.range);
+        foreach (Collider curr in hitColliders)
+        {
+            if (curr.TryGetComponent(out NavNode currentNavNode))
+            {
+                nodes.Add(currentNavNode);
+            }
+        }
+
+        return nodes;
+    }
+
+
 }
